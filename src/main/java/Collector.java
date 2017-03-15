1f51b1d6fb2c931ea99e4bcb4342bdb736fc8db0
Victor <shinbuev@gmail.com>
2017-03-15 15:13:38 +0200

    ThreadPoll using
    ThreadPoll using, collect to synchronizedList

--- src/main/java/Collector.java
+++ src/main/java/Collector.java
@@ -1,28 +1,69 @@
 import org.xbill.DNS.*;
 
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Victor.S on 14-Mar-17.
  */
-public class Collector extends Thread {
+public class Collector implements Runnable {
 
-    private ArrayList<String> domains;
+    private static ArrayList<String> domains = new ArrayList<>();
+    private static ArrayList<String> results = new ArrayList<>();
+    private static List<String> synchronizedList =  Collections.synchronizedList(results);
 
-    public Collector(String name, ArrayList<String> domains) throws FileNotFoundException {
-        super(name);
-        this.domains = domains;
+    private AtomicInteger count = new AtomicInteger();
+
+    public static void main(String[] args) throws IOException {
+
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            while ((line = br.readLine()) != null) {
+                domains.add(line);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(100);Executors.
+
+        for (int i = 0; i < 100; i++){
+            Runnable collector = new Collector();
+            executor.execute(collector);
+        }
+        executor.shutdown();
+
+        while (!executor.isTerminated()) {}
+
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("out.txt", true)));
+            for(String s : synchronizedList) {
+                out.println(s);
+            }
+            out.close();
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
+
+        System.out.println("Finished all threads");
     }
 
     public void run() {
-        for (String domain : domains){
-            System.out.println();
-            String result = domain + System.lineSeparator() + getMx(domain) + System.lineSeparator() + getNs(domain);
+        while (domains.size() != 0){
+            String dom = domains.remove(0);
+            count.getAndIncrement();
+            String result = dom + System.lineSeparator() + getMx(dom) + System.lineSeparator() + getNs(dom);
+//            SingletonFileWriter.getInstance().writeToFile(result);
+            synchronizedList.add(result);
             System.out.println(result);
-            SingletonFileWriter.getInstance().writeToFile(result);
         }
-        System.out.println("Thread " + Thread.currentThread().getName() + " done!");
+
+        System.out.println("Thread " + Thread.currentThread().getName() + " done and made " + count.get() + " domains!");
     }
 
     public String getMx(String domain) {
